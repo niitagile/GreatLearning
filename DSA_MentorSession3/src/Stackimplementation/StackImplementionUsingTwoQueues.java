89c5432ef233e4ee2f2b79b49f529f46921cfc95
niitagile <43024343+niitagile@users.noreply.github.com>
2022-04-24 10:37:31 +0530

    Add files via upload

---
+++ DSA_MentorSession3/src/Stackimplementation/StackImplementionUsingTwoQueues.java
@@ -0,0 +1,51 @@
+package Stackimplementation;
+import java.util.Scanner;
+
+
+public class StackImplementionUsingTwoQueues {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		StackUsingQueue stackUsingQueue = new StackUsingQueue();
+		System.out.println("Stack Using Two Queues \n");
+		char ch;
+		do {
+			System.out.println("Stack Operations");
+			System.out.println("1. push");
+			System.out.println("2. pop");
+			System.out.println("3. check empty");
+			System.out.println("4. size");
+			int choice = scanner.nextInt();
+			switch (choice) {
+			case 1:
+				System.out.println("Enter integer element to push");
+				
+				
+				stackUsingQueue.push(scanner.nextInt());
+				break;
+			case 2:
+				try {
+					System.out.println("Popped Element = " + stackUsingQueue.pop());
+				} catch (Exception e) {
+					System.out.println("Error : " + e.getMessage());
+				}
+				break;
+			case 3:
+				System.out.println("Empty status = " + stackUsingQueue.isEmpty());
+				break;
+			case 4:
+				System.out.println("Size = " + stackUsingQueue.getSize());
+				break;
+			default:
+				System.out.println("Enter a valid option \n ");
+				break;
+			}
+			
+			stackUsingQueue.display();
+			System.out.println("Do you want to continue (Type y or n) \n");
+			ch = scanner.next().charAt(0);
+		} while (ch == 'Y' || ch == 'y');
+		scanner.close();
+	}
+	
+}
+
