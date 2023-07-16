5cabdaaf737fb19cb79a929d63cc8b1dd4df330a
niitagile <43024343+niitagile@users.noreply.github.com>
2023-07-16 18:54:35 +0530

    Update index.js

--- AdvanceJavaScript/index.js
+++ AdvanceJavaScript/index.js
@@ -401,81 +401,317 @@ const foodItem= [
 },
 ]
 
+
+
 //add address
 function addAddress(){
-    var address=prompt('Enter your address');
+    var address= prompt('Enter your address','');
     if(address)
-    document.getElementById('add-address').innerText=address;
+    document.getElementById('add-address').innerText= address;
     else
-    alert("Address not added")
+    alert("Address not added");
 }
 
 document.getElementById('add-address').addEventListener('click',addAddress);
+//add event in mobile version
+document.getElementById('m-add-address').addEventListener('click',addAddress);
 
-//Logic for items to display on the page
+
+// Logic for items to display on the page
 function displayItems(){
-    //fetch all category elements from html by using getElementById
-    var biryani=document.getElementById('biryani');
-    var paneer=document.getElementById('paneer');
-    var chicken=document.getElementById('chicken');
-    var vegetable=document.getElementById('vegetable');
-    var chinese=document.getElementById('chinese');
-    var southIndian=document.getElementById('south-indian');
+// fetch all category elements from html by using getElemetById
+    var biryani= document.getElementById('biryani');
+    var paneer=  document.getElementById('paneer');;
+    var chicken=  document.getElementById('chicken');
+    var vegetable=  document.getElementById('vegetable');
+    var chinese=  document.getElementById('chinese');
+    var southIndian=  document.getElementById('south-indian');
+    //Array of all categories name
+    catList = [biryani,chicken,paneer,vegetable,chinese,southIndian]
 
-    //Array of Categories
-    catList=[biryani,paneer,chicken,vegetable,chinese,southIndian];
-
-    //Fetch data from foodItem array and store them in categorieswise array
-    const biryaniData=foodItem.filter((item)=>item.category=='biryani');
-    const paneerData=foodItem.filter((item)=>item.category=='paneer');
-    const chickenData=foodItem.filter((item)=>item.category=='chicken');
-    const vegetableData=foodItem.filter((item)=>item.category=='vegetable');
-    const chineseData=foodItem.filter((item)=>item.category=='chinese');
-    const southData=foodItem.filter((item)=>item.category=='south indian');
-    fooddata=[biryaniData,paneerData,chickenData,vegetableData,chineseData,southData];
-    //create different elements to show items one by one
-    fooddata.forEach((foodCategory,idx)=>{
+    //Fetch data from fooditem array and store it in category array
+    const biryaniData= foodItem.filter((item)=>item.category=='biryani');
+    const chickenData= foodItem.filter((item)=>item.category=='chicken');
+    const PaneerData= foodItem.filter((item)=>item.category=='paneer');
+    const vegetableData= foodItem.filter((item)=>item.category=='vegetable');
+    const chineseData= foodItem.filter((item)=>item.category=='chinese');
+    const southData= foodItem.filter((item)=>item.category=='south indian');
+    
+    [biryaniData,chickenData,PaneerData,vegetableData,chineseData,southData].forEach((foodCategory,idx) => {
         foodCategory.map(item=>{
-            //create a dv to display food item
-            var itemCard=document.createElement('div');
+            //create a div to display food item
+            var itemCard= document.createElement('div');
             itemCard.setAttribute('id','item-card');
-            //Another div inside item-card div
-            var cardTop=document.createElement('div');
+               //Another div inside food item div 
+            var cardTop= document.createElement('div');
             cardTop.setAttribute('id','card-top');
-            //create element to display rating
-            var star=document.createElement('i');
-            star.setAttribute('id','rating');
+           //create element to display rating
+            var star= document.createElement('i');
             star.setAttribute('class','fa fa-star');
+            star.setAttribute('id','rating');
             star.innerText=''+item.rating;
-            //create element to display heart
-            var heart=document.createElement('i');
+            //Create heart 
+            var heart= document.createElement('i');
+            heart.setAttribute('class','fa fa-heart-o add-to-cart');
             heart.setAttribute('id',item.id);
-            star.setAttribute('class','fa fa-heart-o add-to-cart');
+                //add child in cardtop
             cardTop.appendChild(star);
             cardTop.appendChild(heart);
-            itemCard.appendChild(cardTop);
-            //Add image
-            var img=document.createElement('img');
-            img.src=item.img;
-            itemCard.appendChild(img);
-            //Add name of item
-            var itemName=document.createElement('p');
-            itemName.setAttribute('id','item-name');
-            itemName.innerText=item.name;
-            itemCard.appendChild(itemName);
-            //Add item price
-            var itemPrice=document.createElement('p');
-            itemPrice.setAttribute('id','item-price');
-            itemPrice.innerText='Price : $ '+item.price;
-            itemCard.appendChild(itemName);
 
-            //add itemCard in catList
+            //Add image
+            var img= document.createElement('img');
+            img.src=item.img;
+
+            //Add name of item
+            var itemName= document.createElement('p');
+            itemName.setAttribute('id','item-name');
+            itemName.innerText= item.name;
+            //Add price of item
+            var itemPrice= document.createElement('p');
+            itemPrice.setAttribute('id','item-price');
+            itemPrice.innerText= 'Price : $ ' + item.price;
+
+         // add child in  itemcard
+         
+         itemCard.appendChild(cardTop);
+            itemCard.appendChild(img);
+            itemCard.appendChild(itemName);
+            itemCard.appendChild(itemPrice);
+
             catList[idx].appendChild(itemCard);
-           
+
+        
+
+
+    })
+});
+    
+}
+displayItems();
+
+
+const QuickNavigate= [...new Map(foodItem.map(item=> [item['category'],item])).values()];
+
+// Logic to create navigate List for each category
+function selectTaste(){
+    var categoryList= document.getElementById('category-list');
+
+    QuickNavigate.map(item=>{
+        
+        var listCard= document.createElement('div');
+        listCard.setAttribute('class','list-card');
+    
+        var listImg= document.createElement('img');
+        listImg.src= item.img;
+    
+        var listName= document.createElement('a');
+        listName.setAttribute('class','list-name');
+        listName.innerText= item.category;
+        listName.setAttribute('href','#'+item.category)
+    
+        listCard.appendChild(listImg);
+        listCard.appendChild(listName);
+
+        var cloneListCard= listCard.cloneNode(true);
+        categoryList.appendChild(listCard);
+        document.querySelector('.category-header').appendChild(cloneListCard)
+    })
+}
+selectTaste();
+
+document.querySelectorAll('.add-to-cart').forEach(item=>{
+    item.addEventListener('click',addToCart)
+})
+
+//cartdata will conatin all items selected by use to purchase
+var cartData= [];
+function addToCart(){
+
+    var itemToAdd= this.parentNode.nextSibling.nextSibling.innerText;
+    var itemObj= foodItem.find(element=>element.name==itemToAdd);
+    var index= cartData.indexOf(itemObj);
+    if(index=== -1){
+        document.getElementById(itemObj.id).classList.add('toggle-heart');
+        cartData= [...cartData,itemObj];
+    }
+        else if(index > -1){
+            alert("Added to cart!");
+        }
+        document.getElementById('cart-plus').innerText= ' ' + cartData.length + ' Items';
+        document.getElementById('m-cart-plus').innerText=' ' + cartData.length;
+        totalAmount();
+        cartItems();
+    }
+
+
+
+
+
+function cartItems(){
+    var tableBody=  document.getElementById('table-body');
+    tableBody.innerHTML= '';
+    cartData.map(item=>{
+        var tableRow= document.createElement('tr');
+        
+        var rowData1= document.createElement('td');
+        var img= document.createElement('img');
+        img.src= item.img;
+        rowData1.appendChild(img);
+
+        var rowData2= document.createElement('td');
+        rowData2.innerText= item.name;
+
+        var rowData3= document.createElement('td');
+        var btn1= document.createElement('button');
+        btn1.setAttribute('class','decrease-item');
+        btn1.innerText= '-';
+        var span= document.createElement('span');
+        span.innerText= item.quantity;
+        var btn2= document.createElement('button');
+        btn2.setAttribute('class','increase-item');
+        btn2.innerText= '+';
+        
+        rowData3.appendChild(btn1);
+        rowData3.appendChild(span);
+        rowData3.appendChild(btn2);
+
+        var rowData4= document.createElement('td');
+        rowData4.innerText= item.price;
+    
+        tableRow.appendChild(rowData1);
+        tableRow.appendChild(rowData2);
+        tableRow.appendChild(rowData3);
+        tableRow.appendChild(rowData4);
+    
+        tableBody.appendChild(tableRow);
+        document.querySelectorAll('.increase-item').forEach(item=>{
+            item.addEventListener('click',incrementItem)
+        })
+    
+        document.querySelectorAll('.decrease-item').forEach(item=>{
+            item.addEventListener('click',decrementItem)
         })
 
     });
 
-}
-displayItems();
 
+}
+
+function incrementItem(){
+    let itemToInc= this.parentNode.previousSibling.innerText;
+    var incObj= cartData.find(element=>element.name==itemToInc);
+    incObj.quantity+=1;
+    currPrice= (incObj.price*incObj.quantity - incObj.price*(incObj.quantity-1))/(incObj.quantity-1);
+    incObj.price= currPrice*incObj.quantity;
+    totalAmount()
+    cartItems();
+
+
+
+}
+
+
+var currPrice= 0;
+function decrementItem(){
+    let itemToInc= this.parentNode.previousSibling.innerText;
+    let decObj= cartData.find(element=>element.name==itemToInc);
+    let ind= cartData.indexOf(decObj);
+    if(decObj.quantity >1){
+        currPrice= (decObj.price*decObj.quantity - decObj.price*(decObj.quantity-1))/(decObj.quantity);
+        decObj.quantity-= 1;
+        decObj.price= currPrice*decObj.quantity;
+    }
+    else{
+        cartData.splice(ind,1);
+        document.getElementById(decObj.id).classList.remove('toggle-heart')
+        document.getElementById('cart-plus').innerText= ' ' + cartData.length + ' Items';
+        if(cartData.length < 1 && flag){
+            document.getElementById('food-items').classList.toggle('food-items');
+            document.getElementById('category-list').classList.toggle('food-items');
+            document.getElementById('m-cart-plus').classList.toggle('m-cart-toggle')
+            document.getElementById('cart-page').classList.toggle('cart-toggle');
+            document.getElementById('category-header').classList.toggle('toggle-category');
+            document.getElementById('checkout').classList.toggle('cart-toggle');
+            flag= false;
+            alert("Currently no item in cart!");
+            console.log(flag)
+        }
+    
+    }
+  totalAmount()
+    cartItems()
+}
+
+//to add all css after adding items in Cart
+var flag= false;
+function cartToggle(){
+    if(cartData.length > 0){
+        document.getElementById('food-items').classList.toggle('food-items');
+        document.getElementById('category-list').classList.toggle('food-items');
+        document.getElementById('category-header').classList.toggle('toggle-category');
+        document.getElementById('m-cart-plus').classList.toggle('m-cart-toggle')
+        document.getElementById('cart-page').classList.toggle('cart-toggle');
+        document.getElementById('checkout').classList.toggle('cart-toggle');
+        flag= true;
+        console.log(flag)
+    }
+    else{
+        alert("Currently no item in cart!");
+    }
+}
+
+document.getElementById('cart-plus').addEventListener('click',cartToggle);
+document.getElementById('m-cart-plus').addEventListener('click',cartToggle);
+
+//Calculate total amount of each item in cartlist
+function totalAmount(){
+    var sum=0;
+    cartData.map(item=>{
+        sum+= item.price;
+    })
+    document.getElementById('total-item').innerText= 'Total Item : ' + cartData.length;
+    document.getElementById('total-price').innerText= 'Total Price : $ ' + sum;
+    document.getElementById('m-total-amount').innerText= 'Total Price : $ ' + sum;
+}
+
+function addEvents(){
+    document.querySelectorAll('.add-to-cart').forEach(item=>{
+        item.addEventListener('click',addToCart)
+    });
+    document.querySelectorAll('.increase-item').forEach(item=>{
+        item.addEventListener('click',incrementItem)
+    })
+    document.querySelectorAll('.decrease-item').forEach(item=>{
+        item.addEventListener('click',decrementItem)
+    })
+    document.querySelectorAll('.cart-btn').forEach(item=>{
+        item.addEventListener('click',()=>{
+            cartData=[]
+            alert("Order placed successfully");
+            window.location.reload();
+        })
+    })
+}
+
+window.onresize= window.onload= function(){
+    var size= window.screen.width;
+    if(size<800){
+        var cloneFoodItems= document.getElementById('food-items').cloneNode(true);
+        var cloneCartPage= document.getElementById('cart-page').cloneNode(true);
+        document.getElementById('food-items').remove();
+        document.getElementById('cart-page').remove();
+        document.getElementById('category-header').after(cloneFoodItems);
+        document.getElementById('food-items').after(cloneCartPage);
+        addEvents()
+    }
+    if(size>800){
+        var cloneFoodItems= document.getElementById('food-items').cloneNode(true);
+        document.getElementById('food-items').remove();
+        document.getElementById('header').after(cloneFoodItems);
+
+        var cloneCartPage= document.getElementById('cart-page').cloneNode(true);
+        document.getElementById('cart-page').remove();
+        document.getElementById('food-items').after(cloneCartPage);
+        addEvents()
+    }
+}
